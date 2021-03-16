862982a6eef2e139cc28e9ce3317ddf7c920a224
poppelee <ehpoppele@gmail.com>
2021-03-16 16:58:41 -0700

    bash test now working with pexpect and stockfish

--- bash_test.py
+++ bash_test.py
@@ -1,15 +1,19 @@
-import subprocess
+import pexpect
 import time
 
 
 if __name__ == "__main__":
-    stockfish = subprocess.Popen(["./stockfish"])
-    outs, errs = stockfish.communicate("setoption name Threads value 24")
-    outs, errs = stockfish.communicate("position fen r1bqkb1r/pppp1ppp/2n2n2/4p1N1/2B1P3/8/PPPP1PPP/RNBQK2R b KQkq - 5 4")
-    print(errs)
+    stockfish = pexpect.spawn("./stockfish")
+    stockfish.expect('')
+    stockfish.sendline("setoption name Threads value 24 position fen r1bqkb1r/pppp1ppp/2n2n2/4p1N1/2B1P3/8/PPPP1PPP/RNBQK2R b KQkq - 5 4")
+    stockfish.expect('')
     start = time.perf_counter()
-    outs, errs  = stockfish.communicate("go btime 10000")
+    stockfish.sendline("go btime 50000")
+    stockfish.expect("bestmove .*")
     elapsed = time.perf_counter() - start
     print(elapsed)
-    print(outs)
-    print("error ret is:", errs)
+    bytes = stockfish.after
+    string = bytes.decode('utf-8')
+    print(string)
+    move = string.split()[1]
+    print(move)
