3b5f4f35e140fe2d19e26e435f9f5d47edc90438
poppelee <ehpoppele@gmail.com>
2021-04-05 08:42:51 -0700

    Main program done

--- ChessTime.py
+++ ChessTime.py
@@ -5,8 +5,8 @@ from TerminalEngine import *
 from Board import *
 
 #Defining the data/defaults for each component
-bf1_data = {'ip':'192.168.100.2', 'port':4321, 'program':'stockfish_bf1', 'threads':'16', 'wattage':1 }
-cpu_data = {'ip':'192.168.100.1', 'port':4321, 'program':'stockfish_cpu', 'threads':'24', 'wattage':1 }
+bf1_data = {'ip':'192.168.100.2', 'port':4321, 'program':'stockfish_bf1', 'threads':'16', 'wattage':(1/11.5) }
+cpu_data = {'ip':'192.168.100.1', 'port':4321, 'program':'stockfish_cpu', 'threads':'24', 'wattage':(1/93) }
 ngd_data = {'ip':'192.168.100.2', 'port':4321, 'wattage':1 } 
 sys_data = {'bf1':bf1_data, 'chimera':cpu_data, 'ngd':ngd_data}
 
@@ -19,65 +19,79 @@ def clockTime(ms):
     return (h + ':' + m + ':' + s + '.' + f)
 
 if __name__ == "__main__":
-    #Sys args are number of games, name of starting side of player (w/b), opponent to connect to, and optional command to suppress printing
-    if len(sys.argv) < 4:
-        print("Missing an arg")
+    #Sysargs are opponent to connect and number of games, then two optional for hosting and suppressing printing, taking 'host' 'noprint' or just 'True/true' and 'False/false', respectively
+    if len(sys.argv) < 3:
+        print("Missing a mandatory argument")
         assert False
-    num_games = sys.argv[1]
-    side = sys.argv[2]
-    opnt_data = sys_data[sys.argv[3]]
+    opnt_data = sys_data[sys.argv[1]]
+    num_games = int(sys.argv[2])
     self_data = sys_data[socket.gethostname()]
     engine = launchEngine(self_data['program'], self_data['threads'])
     #ip_addr = opnt_data['ip']
     #port = opnt_data['port']
 
+    host = False
+    host_args = ['host', 'True', 'true', 'T', 't']
+    if len(sys.argv) >= 4 and (sys.argv[3] in host_args):
+        host = True
+
     print_game = True
-    if len(sys.argv) >= 5 and sys.argv[4] == 'noprint':
+    print_args = ['noprint', 'no_print', 'NoPrint', 'Noprint', 'False', 'false', 'F', 'f']
+    if len(sys.argv) >= 5 and (sys.argv[4] in print_args):
         print_game = False
+
     outcomes = []
+    std_time = 60000 * 60
+    side = ''
+    self_time = std_time *self_data['wattage']
+    opnt_time = std_time *opnt_data['wattage']
 
     listen_socket = None
     game_socket = None
-    if side == 'w':
-        w_time = w_time * self_data['wattage']
-        b_time = w_time * opnt_data['wattage']
+    if host:
+        #w_time = w_time * self_data['wattage']
+        #b_time = w_time * opnt_data['wattage']
         listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         listen_socket.bind((self_data['ip'], self_data['port']))#Currently just localhost connection
         listen_socket.listen()
         game_socket, addr = listen_socket.accept()
         print("Connection established by ", addr)
-    elif side == 'b':
-        b_time = w_time * self_data['wattage']
-        w_time = w_time * opnt_data['wattage']
+        side = 'b'
+    elif not host:
+        #b_time = w_time * self_data['wattage']
+        #w_time = w_time * opnt_data['wattage']
         game_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         game_socket.connect((opnt_data['ip'], opnt_data['port']))
         print("Connection established to host")
+        side = 'w'
     else:
         print("No valid argument for player side")
         assert False
     for i in range(num_games):
-        if i%2 == 1:
-            if side == 'w':
-                side = 'b'
-            else:
-                side = 'w'
-        #Reset state
+        #Swap sides and reset state
+        if side == 'w':
+            side = 'b'
+            b_time = self_time
+            w_time = opnt_time
+        else:
+            side = 'w'
+            w_time = self_time
+            b_time = opnt_time
         board = Board()
-        b_time = 60000
-        w_time = 60000
         game_over = False
         #make first move
-        fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
-        my_move, time_used = getMove(engine, fen, b_time, w_time)
-        fen, algebraic = board.playMove(my_move)
-        if print_game:
-            print(algebraic)
-        w_time -= time_used
-        if w_time <= 0:
-            print("You fool! You used up all your time on your first move!")
-            assert False
-        data = bytes(my_move + ' ' + str(time_used), 'utf-8')
-        game_socket.sendall(data)
+        if side == 'w':
+            fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+            my_move, time_used = getMove(engine, fen, b_time, w_time)
+            fen, algebraic = board.playMove(my_move)
+            if print_game:
+                print(algebraic)
+            w_time -= time_used
+            if w_time <= 0:
+                print("You fool! You used up all your time on your first move!")
+                assert False
+            data = bytes(my_move + ' ' + str(time_used), 'utf-8')
+            game_socket.sendall(data)
         while not(game_over):
             data = game_socket.recv(1024)
             opponent_move = data.decode('utf-8').split()[0]
